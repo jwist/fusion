4d2b62a8ebde75696ea13367f4f94b87c83333f1
jul <jul@anpc3>
2020-12-12 11:07:43 +0800

    fix: correction ton matchALl to resolve #2

--- R/matchAll.R
+++ R/matchAll.R
@@ -7,7 +7,7 @@
 matchAll <- function(list) {
   res <- lapply(list[-1],
                 function(x) matchPairwise(list[[1]], x)[[2]])
-  firstEl <- matchPairwise(list[[1]], list[[2]])[1]
+  firstEl <- matchPairwise(list[[1]], res[[1]])[1]
   res <- c(firstEl, res)
   return(res)
 
