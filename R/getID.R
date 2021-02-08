516937dcd7ebbd4e2b923ab0f24746979a5147bc
jul <jul@anpc3>
2021-02-08 10:28:08 +0800

    fix: resolve issue #9

--- R/getID.R
+++ R/getID.R
@@ -2,11 +2,12 @@
 #'
 #' @param da dataElement
 #' @param using the name of the column used as UID
+#' @param re.rm if TRUE replicate markers are removed
 #' @return the sampleID
 #'
 #' @export
 #'
-setGeneric("getID", function(da, using = "sampleID") {
+setGeneric("getID", function(da, using = "sampleID", re.rm = FALSE) {
   standardGeneric("getID")
 })
 
@@ -14,14 +15,18 @@ setGeneric("getID", function(da, using = "sampleID") {
 #'
 #' @param da dataElement
 #' @param using the name of the column used as UID
+#' @param re.rm if TRUE replicate markers are removed
 #' @return the sampleID
 #'
 #' @export getID
 #' @export
 setMethod("getID",
           c(da = "dataElement"),
-          function(da, using = "sampleID") {
+          function(da, using = "sampleID", re.rm = FALSE) {
             sampleID <- unlist(unname(da@obsDescr[[1]][using]))
+            if (re.rm) {
+              sampleID <- gsub("#.", "", sampleID)
+            }
             return(sampleID)
           }
 )
--- tests/testthat/test-getID.R
+++ tests/testthat/test-getID.R
@@ -12,6 +12,36 @@ test_that("sampleID is retrieved", {
   expect_equal(sampleID, seq(1, 10))
 })
 
+test_that("sampleID is retrieved", {
+  x = matrix(rep(c(1:10), 10), 10, 10)
+  ID <- as.character(c(1, seq(1, 9)))
+  param = list(data.frame(sampleID = makeUnique(ID),
+                          sampleType = rep("sample", 10)))
+  da = new("dataElement", x,
+           varName = as.character(seq(1, 10)),
+           type = "NMR",
+           method = "1D",
+           obsDescr = param)
+  sampleID <- getID(da)
+  expect_equal(length(sampleID), 10)
+  expect_equal(sampleID, makeUnique(ID))
+})
+
+test_that("sampleID is retrieved", {
+  x = matrix(rep(c(1:10), 10), 10, 10)
+  ID <- as.character(c(1, seq(1, 9)))
+  param = list(data.frame(sampleID = makeUnique(ID),
+                          sampleType = rep("sample", 10)))
+  da = new("dataElement", x,
+           varName = as.character(seq(1, 10)),
+           type = "NMR",
+           method = "1D",
+           obsDescr = param)
+  sampleID <- getID(da, re.rm = TRUE)
+  expect_equal(length(sampleID), 10)
+  expect_equal(sampleID, ID)
+})
+
 # test_that("UID is retrieved", {
 #   x = matrix(rep(c(1:10), 10), 10, 10)
 #   param = list(data.frame(UID = seq(1, 10),
