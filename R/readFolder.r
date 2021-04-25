fb75c25610e0402f82825e19f109b54f97107e1e
jul <jul@anpc3>
2021-04-25 23:47:47 +0800

    removing buggy test code

--- R/readFolder.r
+++ R/readFolder.r
@@ -13,7 +13,7 @@ readFolder <- function(path) {
         newExp <- readExperiment(file.path(expPath, "11"))
         if (!is.null(newExp)) {
           cpmg <- c(cpmg, list(newExp))
-        } 
+        }
       } else {
         cat(crayon::yellow("fusion::readFolder >>", expPath, "ivdr or lipoproteins not found\n"))
       }
@@ -28,22 +28,22 @@ getValue <- function(list, name) {
   which(d$noesy[[1]]$procs$name == "WDW")
 }
 
-X <- t(sapply(d$noesy, function(x) x$spec$y))
-eretic <- sapply(d$noesy, function(x) getEreticFactor(x$eretic))
-fac <- sapply(eretic, function(x) x[[1]])
-ref <- sapply(eretic, function(x) x[[2]])
-ppm <- d$noesy[[1]]$spec$x
-
-plot(X[1,c(85000:87000)], type = "l")
-matplot(ppm[c(20300:20800)], t(X[,c(20300:20800)]), type = "l")
-matplot(ppm[c(20300:20800)], t(X[,c(20300:20800)] / fac), type = "l")
-
-
-plot(X[1,c(85000:87000)], type = "l")
-matplot(ppm[c(20300:20800)], t(X[,c(20300:20800)]), type = "l")
-matplot(ppm[c(85000:87000)], t(X[,c(85000:87000)] / fac), type = "l")
-
-maximum <- apply(X[,c(20300:20800)] / fac, 1, max)
-plot(maximum)
-# pathToData <- "/home/rstudio/data/imports/data2/BIOGUNE"
-# d <- readFolder(pathToData)
+# X <- t(sapply(d$noesy, function(x) x$spec$y))
+# eretic <- sapply(d$noesy, function(x) getEreticFactor(x$eretic))
+# fac <- sapply(eretic, function(x) x[[1]])
+# ref <- sapply(eretic, function(x) x[[2]])
+# ppm <- d$noesy[[1]]$spec$x
+#
+# plot(X[1,c(85000:87000)], type = "l")
+# matplot(ppm[c(20300:20800)], t(X[,c(20300:20800)]), type = "l")
+# matplot(ppm[c(20300:20800)], t(X[,c(20300:20800)] / fac), type = "l")
+#
+#
+# plot(X[1,c(85000:87000)], type = "l")
+# matplot(ppm[c(20300:20800)], t(X[,c(20300:20800)]), type = "l")
+# matplot(ppm[c(85000:87000)], t(X[,c(85000:87000)] / fac), type = "l")
+#
+# maximum <- apply(X[,c(20300:20800)] / fac, 1, max)
+# plot(maximum)
+# # pathToData <- "/home/rstudio/data/imports/data2/BIOGUNE"
+# # d <- readFolder(pathToData)
