75f4824c7f2acbb394ef343245b1b14eb4c5c28f
julien wist <julien wist@correounivalle.edu.co>
2022-04-07 16:56:19 +0800

    improve getEretic

--- R/getEretic.r
+++ R/getEretic.r
@@ -9,22 +9,28 @@ getEretic <- function(path){
   path <- file.path(path, "QuantFactorSample.xml")
   if (file.exists(path)) {
     xml <- read_xml(path, options = "NOBLANKS")
-    ereticPosition <- as.numeric(xml_text(xml_find_all(xml, ".//Artificial_Eretic_Position")))
-    ereticLineWidth <- as.numeric(xml_text(xml_find_all(xml, ".//Artificial_Eretic_Line_Width")))
-    ereticConcentration <- as.numeric(xml_text(xml_find_all(xml, ".//Artificial_Eretic_Concentration")))
-    tubeID <- xml_attr(xml_find_all(xml, ".//Eretic_Sample_Tube"), "ID")
-    Tmin <- as.numeric(xml_text(xml_find_all(xml, ".//Temperature_min")))
-    Tmax <- as.numeric(xml_text(xml_find_all(xml, ".//Temperature_max")))
-    ereticCalibration <- as.numeric(xml_text(xml_find_all(xml, ".//Eretic_Calibration//Eretic_Factor")))
+    CalEreticPosition <- as.numeric(xml_text(xml_find_all(xml, ".//Artificial_Eretic_Position")))
+    CalEreticLineWidth <- as.numeric(xml_text(xml_find_all(xml, ".//Artificial_Eretic_Line_Width")))
+    CalEreticConcentration <- as.numeric(xml_text(xml_find_all(xml, ".//Artificial_Eretic_Concentration")))
+    CalTubeID <- xml_attr(xml_find_all(xml, ".//Eretic_Sample_Tube"), "ID")
+    CalTmin <- as.numeric(xml_text(xml_find_all(xml, ".//Temperature_min")))
+    CalTmax <- as.numeric(xml_text(xml_find_all(xml, ".//Temperature_max")))
+    CalP1 <- as.numeric(xml_text(xml_find_all(xml, ".//Eretic_Calibration//P1")))
+    CalEreticCalibration <- as.numeric(xml_text(xml_find_all(xml, ".//Eretic_Calibration//Eretic_Factor")))
     ereticFactor <- as.numeric(xml_text(xml_find_all(xml, ".//Application_Parameter//Eretic_Factor")))
-    df <- data.frame(ereticPosition,
-                     ereticLineWidth,
-                     ereticConcentration,
-                     tubeID,
-                     Tmin,
-                     Tmax,
-                     ereticCalibration,
-                     ereticFactor)
+    P1 <- as.numeric(xml_text(xml_find_all(xml, ".//Application_Parameter//P1")))
+    temperature <- as.numeric(xml_text(xml_find_all(xml, ".//Application_Parameter//Temperature")))
+    df <- data.frame(CalEreticPosition,
+                     CalEreticLineWidth,
+                     CalEreticConcentration,
+                     CalTubeID,
+                     CalTmin,
+                     CalTmax,
+                     CalP1,
+                     CalEreticCalibration,
+                     ereticFactor,
+                     temperature,
+                     P1)
     return(df)
   } else {
     cat(crayon::yellow("fusion::getEretic >>", path, "not found\n"))
