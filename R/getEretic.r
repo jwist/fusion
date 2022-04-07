ba37861d667d14a60db7f9f7b7494ce9c72e44c0
julien wist <julien wist@correounivalle.edu.co>
2022-04-07 16:47:22 +0800

    fix eretic

--- R/getEretic.r
+++ R/getEretic.r
@@ -9,12 +9,23 @@ getEretic <- function(path){
   path <- file.path(path, "QuantFactorSample.xml")
   if (file.exists(path)) {
     xml <- read_xml(path, options = "NOBLANKS")
-    # assign("content", list(), envir = .GlobalEnv)
-    content <- list()
-    readXML(xml)
-    quant <- data.frame(do.call(rbind, content))
-    # rm("content", envir = .GlobalEnv)
-    return(quant)
+    ereticPosition <- as.numeric(xml_text(xml_find_all(xml, ".//Artificial_Eretic_Position")))
+    ereticLineWidth <- as.numeric(xml_text(xml_find_all(xml, ".//Artificial_Eretic_Line_Width")))
+    ereticConcentration <- as.numeric(xml_text(xml_find_all(xml, ".//Artificial_Eretic_Concentration")))
+    tubeID <- xml_attr(xml_find_all(xml, ".//Eretic_Sample_Tube"), "ID")
+    Tmin <- as.numeric(xml_text(xml_find_all(xml, ".//Temperature_min")))
+    Tmax <- as.numeric(xml_text(xml_find_all(xml, ".//Temperature_max")))
+    ereticCalibration <- as.numeric(xml_text(xml_find_all(xml, ".//Eretic_Calibration//Eretic_Factor")))
+    ereticFactor <- as.numeric(xml_text(xml_find_all(xml, ".//Application_Parameter//Eretic_Factor")))
+    df <- data.frame(ereticPosition,
+                     ereticLineWidth,
+                     ereticConcentration,
+                     tubeID,
+                     Tmin,
+                     Tmax,
+                     ereticCalibration,
+                     ereticFactor)
+    return(df)
   } else {
     cat(crayon::yellow("fusion::getEretic >>", path, "not found\n"))
   }
