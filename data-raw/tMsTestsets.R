397908d750dca5e30a04d8346af14639a0402545
julien wist <julien wist@correounivalle.edu.co>
2022-05-25 17:12:34 +0800

    make mv external

--- data-raw/tMsTestsets.R
+++ data-raw/tMsTestsets.R
@@ -4,4 +4,4 @@ file = "./inst/tryptophanPathwayMolecularWeights.txt"
 mw = read.table(file, header = TRUE, sep = "\t", dec = ".")
 
 tMsTestsets <- list(mw = mw)
-usethis::use_data(tMsTestsets, overwrite = TRUE, internal = TRUE)
+usethis::use_data(tMsTestsets, overwrite = TRUE)
