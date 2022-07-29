bbf5aa941d43656aaa734e4920cd38f4c62603bd
julien wist <julien wist@correounivalle.edu.co>
2022-07-29 12:48:28 +0800

    adding data.table dependency and adding data.table in readSpectrum

--- R/readSpectrum.R
+++ R/readSpectrum.R
@@ -8,49 +8,86 @@
 #' @export
 readSpectrum <- function(path, procs = TRUE, options = list()){
   path1r <- file.path(path, "pdata", "1", "1r")
+
   if (file.exists(path1r)) {
     if (is.logical(procs) && isTRUE(procs)) {
       pathProcs <- file.path(path, "pdata", "1", "procs")
-      if(readParam(pathProcs, "BYTORDP") == 0) {
-        endian <- "little"
-      } else {
-        endian = "big"
-      }
-      nc <- readParam(pathProcs, "NC_proc")
-      size <- readParam(pathProcs, "FTSIZE")
-      y <- read1r(path1r, size, nc, endian)
-      sf <-readParam(pathProcs, "SF")
-      sw <- readParam(pathProcs, "SW_p") / sf
-      inc <- sw/(length(y) - 1)
-      offset <- readParam(pathProcs, "OFFSET")
-      x <- seq(from = offset, to = (offset - sw), by = -inc)
     } else {
-      if(getParam(procs, "BYTORDP") == 0) {
-        endian <- "little"
-      } else {
-        endian = "big"
-      }
-      nc <- getParam(procs, "NC_proc")
-      size <- getParam(procs, "FTSIZE")
-      y <- read1r(path1r, size, nc, endian)
-      sf <- getParam(procs, "SF")
-      sw <- getParam(procs, "SW_p") / sf
-      inc <- sw/(length(y) - 1)
-      offset <- getParam(procs, "OFFSET")
-      x <- seq(from = offset, to = (offset - sw), by = -inc)
+      pathProcs <- procs
     }
+
+    if(readParam(pathProcs, "BYTORDP") == 0) {
+      endian <- "little"
+    } else {
+      endian = "big"
+    }
+
+    nc <- readParam(pathProcs, "NC_proc")
+    size <- readParam(pathProcs, "FTSIZE")
+    y <- read1r(path1r, size, nc, endian)
+    sf <-readParam(pathProcs, "SF")
+    sw <- readParam(pathProcs, "SW_p") / sf
+    inc <- sw/(length(y) - 1)
+    offset <- readParam(pathProcs, "OFFSET")
+    x <- seq(from = offset, to = (offset - sw), by = -inc)
+
+    # applying eretic correction if provided
     if ("ereticFactor" %in% names(options)) {
-      spec <- data.frame(x, y / options$ereticFactor)
-      cat(crayon::blue("fusion::readSpectrum >> spectra corrected for eretic (",
+      y <- y / options$ereticFactor
+
+      cat(crayon::blue("fusion::readSpectrum >> spectra corrected for eretic:",
                        options$ereticFactor,
-                       ")\n"))
-    } else {
-      spec <- data.frame(x, y)
+                       "\n"))
     }
+
+    # correcting for offset if provided (useful for JEDI experiments)
+    if ("offset" %in% names(options)) {
+      x <- x + options$offset
+
+      cat(crayon::blue("fusion::readSpectrum >> spectra corrected for offset:",
+                       options$offset,
+                       "\n"))
+    }
+
+    # if upper and lower bounds are provided the spectra is extrapolated to fit
+    # those boundaries. If no length.out is provided, them similar length is
+    # used.
+    if ("fromTo" %in% names(options)) {
+      from <- options$fromTo[1]
+      to <- options$fromTo[2]
+
+      if ("length.out" %in% names(options)) {
+        length.out <- options$length.out
+      } else {
+        fi <- s$x > from & s$x < to
+        length.out <- sum(fi)
+      }
+
+      newX <- seq(from,
+                  to,
+                  length.out = length.out)
+
+      y <- interp1(x = x,
+                     y = y,
+                     xi = newX,
+                     method = "spline")
+
+      x <- newX
+
+      cat(crayon::blue("fusion::readSpectrum >> spectra in common grid (from:",
+                       from,
+                       "to:",
+                       to,
+                       "dimension:",
+                       length.out,
+                       ")\n"))
+    }
+
+    spec <- data.table(x, y)
     return(spec)
+
   } else {
     cat(crayon::yellow("fusion::readSpectrum >> data not found for", path, "\n"))
   }
 }
-
 
