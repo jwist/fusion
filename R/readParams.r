9546568b1a0140f8f0ba09aa0e51d435ab139071
julien wist <julien wist@correounivalle.edu.co>
2022-10-06 14:50:41 +0800

    fixing readParams

--- R/readParams.r
+++ R/readParams.r
@@ -1,54 +1,55 @@
 
 readParams <- function(filePath) {
   if (file.exists(filePath)) {
-  buf <- file(filePath, open = "r")
-  txt <- readLines(buf, n = -1, warn = FALSE)
-  close(buf)
-  content <- list()
-  counter <- 1
-  while (counter < length(txt)) {
-    line <- txt[[counter]]
-    if (grepl("^##END=", line)) {
-      break
-    }
-    path <- strsplit(filePath, "/")[[1]]
-    if (grepl("^##[A-Z]", line)) {
-      param <- strsplit(line, "= ")[[1]]
-      content <- c(content, list(c(path = path[length(path)],
-                                   name = gsub("##", "", param[1]),
-                                   value = param[2])))
-    }
-    if (grepl("^##\\$", line)) {
-      param <- strsplit(line, "= ")[[1]]
-      value <- gsub("[<->]", "", param[2])
-      subcounter <- 1
-      while (!grepl("^##", txt[[counter + 1]])) {
-        counter <- counter + 1
-        value <- paste(value, txt[[counter]])
+    buf <- file(filePath, open = "r")
+    txt <- readLines(buf, n = -1, warn = FALSE)
+    close(buf)
+    content <- list()
+    counter <- 1 # line counter
+    while (counter < length(txt)) {
+      line <- txt[[counter]]
+      if (grepl("^##END=", line)) {
+        break
       }
-      value <- strsplit(value, " ")
-      if (length(value[[1]]) > 1) {
-        for (i in 2:length(value[[1]])) {
-          content <- c(content, list(c(path = unlist(path[length(path)]),
-                                       name = unlist(paste0(gsub("##\\$", "", param[1]), "_", i - 2)),
-                                       value = unlist(value[[1]][i]))))
+      path <- strsplit(filePath, "/")[[1]]
+      # get titles
+      if (grepl("^##[A-Z]", line)) {
+        param <- strsplit(line, "= ")[[1]]
+        content <- c(content, list(c(path = path[length(path)],
+                                     name = gsub("##", "", param[1]),
+                                     value = param[2])))
+      }
+      # get params
+      if (grepl("^##\\$", line)) {
+        param <- strsplit(line, "= ")[[1]]
+        # value <- gsub("[<->]", "", param[2])
+        value <- param[2]
+        # look for vectors
+        if (grepl("\\([0-9]+..[0-9]+\\)", value)) {
+          counter <- counter + 1
+          vect <- txt[[counter]]
+          value <- strsplit(vect, " ")
+          for (i in 1:length(value[[1]])) {
+            content <- c(content, list(c(path = path[length(path)],
+                                         name = paste0(gsub("##\\$", "", param[1]), "_", i - 1),
+                                         value = value[[1]][i])))
+          }
+        } else {
+          content <- c(content, list(c(path = path[length(path)],
+                                       name = gsub("##\\$", "", param[1]),
+                                       value = gsub("[<->]", "", value))))
         }
-      } else {
-        content <- c(content, list(c(path = unlist(path[length(path)]),
-                                     name = unlist(gsub("##\\$", "", param[1])),
-                                     value = unlist(value))))
       }
+      counter <- counter + 1
     }
-    counter <- counter + 1
-  }
-  fi <- sapply(content$value, function(x) {
-    if (identical(x, character(0))) {
-      res <- NA
-    } else {
-      x
-    }
-  })
-  return(data.frame(do.call(rbind, content)))
+    ret <- data.frame(do.call(rbind, content))
+    fi <- sapply(ret$value, function(x) identical(x, character(0)))
+    ret$value[fi] <- NA
+
+    return(data.frame(path = unlist(ret$path),
+                      name = unlist(ret$name),
+                      value = unlist(ret$value)))
+    # return(ret)
   } else {
     cat(crayon::yellow("fusion::getParams >>", filePath, " file not found\n"))
   }
