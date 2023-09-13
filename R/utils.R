978f75672c3fa797dcaed066a4762bca0c4cb3aa
julien wist <julien wist@correounivalle.edu.co>
2023-09-14 07:33:45 +0800

    fix documentation and deps

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,7 @@
 
 #' a function to unlist but replacing empty values by NA to keep the length
 #' @param l - a list
+#' @param unname - boolean switch to unname response (default = true)
 #' @returns a vector
 delist <- function(l, unname = TRUE) {
   l[sapply(l, function(x) length(x)==0L)] <- NA
@@ -14,11 +15,18 @@ delist <- function(l, unname = TRUE) {
 #' a function to get duplicated pairs
 #' @param vector - a vector
 #' @returns a logical vector
+#' @importFrom data.table data.table
 dduplicated <- function(vector) {
   fi <- duplicated(vector) | duplicated(vector, fromLast = TRUE)
   return(fi)
 }
 
+#' a function to show pairs of duplicated items
+#' @param aTable - a table
+#' @param colWithDuplicates = the column with duplicates
+#' @param colToSelect - the column to display
+#' @returns a logical vector
+#' @importFrom data.table data.table
 showDuplicated <- function(aTable, colWithDuplicates, colToSelect) {
   dt <- data.table(aTable)
   sortedTable <- dt(order())
